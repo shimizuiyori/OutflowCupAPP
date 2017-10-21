b604ff869b6e98e9fd937714c2163fa912743e81
1982775886@qq.com <1982775886@qq.com>
2017-10-21 15:39:11 +0800

    初步配置好Corsswalk

--- app/src/main/java/com/china_tanyo/outflowcupapp/MainActivity.java
+++ app/src/main/java/com/china_tanyo/outflowcupapp/MainActivity.java
@@ -3,11 +3,19 @@ package com.china_tanyo.outflowcupapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.xwalk.core.XWalkView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        XWalkView xWalkView = (XWalkView) this.findViewById(R.id.xwalkWebView);
+        xWalkView.load("http://html5test.com/", null);
+
+        //xWalkView.load("file:///android_asset/www/index.html", null);
     }
 }
